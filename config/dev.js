dbbaf22671b1da7c331c4ef22c3007ba0e3929c6
SophieJung7 <36703871+SophieJung7@users.noreply.github.com>
2020-04-02 16:04:46 +0800

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '1031975789503-lkhc1t5otgq15janfofulkcg4rlb74li.apps.googleusercontent.com',
-  googleClientSecret: 'RnXuF8JkCqMRWcWfYv1ebfUm',
-  mongoURI:
-    'mongodb://sophie:2qAt7J5Cg3gWSmWl@cluster0-shard-00-00-ldxhm.mongodb.net:27017,cluster0-shard-00-01-ldxhm.mongodb.net:27017,cluster0-shard-00-02-ldxhm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123'
-};
